62f8b781ae4e5165d0fdfe5a89017024baa0e721
chacha-nyang <p03gh19@gmail.com>
2021-02-12 17:04:04 +0900

    TextRPG2

---
+++ Test_Practice/Chapter1_Test/TextRPG_0.0.02v/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextRPG_0._0._02v
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
